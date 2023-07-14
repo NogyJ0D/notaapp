fe6cac4118ce83c81c0074828699893d69aa0de2
NogyJ0D <valentingiarra2@gmail.com>
2023-07-14 12:51:42 -0300

    proy added

---
+++ src/main/java/com/valentingiarra/notaapp/NotaappApplication.java
@@ -0,0 +1,13 @@
+package com.valentingiarra.notaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valentingiarra/notaapp/NotaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentingiarra.notaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
